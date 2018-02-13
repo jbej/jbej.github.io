369daa8899a3b2468a0653f8813fa7ed7b881112
Justyna Bejgrowicz <justyna.bejgrowicz@interia.pl>
2018-02-13 20:55:53 +0200

    Add file

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+$(function () {
+	$('[data-toggle="tooltip"]').tooltip();
+
+	var offset = 80;
+
+	$('.navbar a').click(function(event) {
+    event.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView();
+    scrollBy(0, -offset);
+	});
+});
